dc671345bda12530d2c1add9231718564c6b8f93
jaxonetic-github <jaxonetic@gmail.com>
2022-02-20 23:33:52 +0000

    Deploying to gh-pages from @ jaxonetic-github/React-Appointment-Setter@aaa4b78dc7b4cddb1ca194bd14b43ccf988659ed 🚀

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-  presets: ['@babel/preset-env', '@babel/preset-react'],
- plugins: [ "@babel/plugin-syntax-jsx","transform-class-properties","@babel/plugin-transform-runtime"],
-  ignore: ["./src/react-date-picker",'./src/stories'],
-
-
-};
